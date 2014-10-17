9588f7ed7da523d472b07c236e259193d03a4c80
Kathryn Hodge <kahodge@vassar.edu>
2014-10-17 11:28:38 -0400

    Starting...

---
+++ PolymorphicSetBags/src/polymorphicsetbags/PolymorphicSetBags.java
@@ -0,0 +1,31 @@
+
+package polymorphicsetbags;
+
+
+public class PolymorphicSetBags {
+
+    
+    // REQUIREMENTS: 
+   
+   // A finite bag is also called a mulitset and is like a set, 
+   // but each element may occur many times. 
+    
+   // Your bags should be polymorphic, i.e. use generics to allow 
+   // any kind of contents. 
+  // By "pure", we mean that operations on the set return new sets and 
+  // do not modify the old set. 
+    
+    
+    // TO DO: 
+    // FIRST CREATE TESTING HMMMMPH. Property-based testing!
+    // USE self-balancing binary search tree
+    // Design API for bags; need for Iteration
+    // Match Up code with tests
+    // Write paper
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
