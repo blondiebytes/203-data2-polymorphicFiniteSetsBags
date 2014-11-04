0b8b9763d0c84156471dde8096d72feaa113c19c
Kathryn Hodge <kahodge@vassar.edu>
2014-11-04 08:38:49 -0500

    Cleaning Up

--- PolymorphicSetBags/src/polymorphicsetbags/SetBag/SetBag_NonEmpty.java
+++ PolymorphicSetBags/src/polymorphicsetbags/SetBag/SetBag_NonEmpty.java
@@ -3,28 +3,9 @@ package polymorphicsetbags.SetBag;
 // REQUIREMENTS: 
 import polymorphicsetbags.Sequence.Sequence;
 import polymorphicsetbags.Sequence.Sequence_Cat;
-import polymorphicsetbags.Sequence.Sequence_Empty;
 import polymorphicsetbags.Sequence.Sequence_NonEmpty;
 import polymorphicsetbags.Sequence.Sequenced;
-
-   // A finite bag is also called a mulitset and is like a set, 
-// but each element may occur many times. 
-// Your bags should be polymorphic, i.e. use generics to allow 
-// any kind of contents. 
-// By "pure", we mean that operations on the set return new sets and 
-// do not modify the old set. 
-// Need to figure out a way to rotate trees
-// TO DO: 
-// FIRST CREATE TESTING HMMMMPH. Property-based testing!
-// USE self-balancing binary search tree
-// Design API for bags; need for Iteration
-// Match Up code with tests
-// Write paper
-// The main methods we are changing are cardinality, member, add, remove
-// IMPLEMENT USING ITERATOR! How to create iterators?
-// Need "Tree Rotation"
-// Right rotation
-// Left rotation
+        
 public class SetBag_NonEmpty<D extends Comparable> implements Bag<D>, Sequenced<D> {
 
     int count;
@@ -107,10 +88,6 @@ public class SetBag_NonEmpty<D extends Comparable> implements Bag<D>, Sequenced<
         }
         return all.toString();
     }
-   
-    
-    // NEED TO WRITE TESTS
-    
     
     public static Bag empty() {
         return new SetBag_Empty();
@@ -144,21 +121,11 @@ public class SetBag_NonEmpty<D extends Comparable> implements Bag<D>, Sequenced<
         }
     }
 
-    // Check if count is 0 for that element
-    // else do what we did before
     public Bag remove(D elt) {
-        if (elt.equals(this.root)) {
-            return new SetBag_NonEmpty(this.root, this.count - 1,
-                    this.left, this.right);
-        } else if (elt.compareTo(this.root) < 0) {
-            return new SetBag_NonEmpty(this.root, this.count, this.left.remove(elt), this.right);
-        } else {
-            return new SetBag_NonEmpty(this.root, this.count, this.left,
-                    this.right.remove(elt));
-        }
+       return removeN(elt, 1);
     }
 
-    // Not working!
+
     public Bag removeN(D elt, int n) {
         if (elt.equals(this.root)) {
             int max = Math.max(0, this.count - n);
@@ -185,17 +152,7 @@ public class SetBag_NonEmpty<D extends Comparable> implements Bag<D>, Sequenced<
     }
 
     public Bag add(D elt) {
-        if (elt.equals(this.root)) {
-            return new SetBag_NonEmpty(this.root, this.count + 1, this.left, this.right);
-        } else {
-            if (elt.compareTo(this.root) < 0) {
-                return new SetBag_NonEmpty(this.root, this.count, this.left.add(elt), this.right);
-            } // If the root is less than the element, then add it to the 
-            // right tree
-            else {
-                return new SetBag_NonEmpty(this.root, this.count, this.left, this.right.add(elt));
-            }
-        }
+       return addN(elt, 1);
     }
 
     public Bag addN(D elt, int n) {
@@ -211,24 +168,6 @@ public class SetBag_NonEmpty<D extends Comparable> implements Bag<D>, Sequenced<
         }
     }
 
-     public Bag rotate(boolean toRight) {
-        if (toRight) {
-            if (!left.isEmptyHuh()) {
-                SetBag_NonEmpty tl = (SetBag_NonEmpty) left;
-                return new SetBag_NonEmpty(tl.root, tl.count, tl.left, new SetBag_NonEmpty(this.root, this.count, right, tl.right));
-            } else {
-                throw new RuntimeException("AVLTree cannot rotate in that direction!");
-            }
-        } else {
-            if (!right.isEmptyHuh()) {
-                SetBag_NonEmpty tr = (SetBag_NonEmpty) right;
-                return new SetBag_NonEmpty(tr.root, tr.count, new SetBag_NonEmpty(root, count,left,tr.left), tr.right);
-            } else {
-                throw new RuntimeException("AVLTree cannot rotate in that direction!");
-            }
-        }
-    }
-    
   
     public Bag union(Bag u) {
         return u.union(left.union(right)).addN(root, this.getCount(root));
@@ -268,43 +207,6 @@ public class SetBag_NonEmpty<D extends Comparable> implements Bag<D>, Sequenced<
         return "[SB:2 Left: "+ this.left.toStringBST() + " Right: " 
                 + this.right.toStringBST() + "Root: " + this.root + " Value: " + this.getCount(root) + "]";
     }
-    
-      // Another way to balance
-//     public Bag balance(Bag tree) {
-//        if (!tree.isEmptyHuh()) {
-//            SetBag_NonEmpty<D> oldTree = (SetBag_NonEmpty) tree;
-//            SetBag_NonEmpty newSet = new SetBag_NonEmpty(oldTree.root, oldTree.count, balance(oldTree.left), balance(oldTree.right));
-//            if ((newSet.left.cardinality() - newSet.right.cardinality()) > 1) {
-//                //left is overweight
-//                if (newSet.left.left.cardinality() > newSet.left.right.cardinality()) {
-//                    //left left case
-//                    newSet = newSet.rotate(true);
-//                } else {
-//                    //left right case
-//                    newSet = new SetBag_NonEmpty (newSet.root, newSet.count, 
-//                            newSet.left.rotate(false)), newSet.right.rotatenewSet.getRight(),
-//                                         newSet.getLeft().rotate(false),
-//                                         newSet.getData()).rotate(true);
-//                }
-//            } else if (newSet.getLeft().height() - newSet.getRight().height() < -1) {
-//                //right is overweight
-//                if (newSet.getRight().getRight().height() > newSet.getRight().getLeft().height()) {
-//                    //right right case
-//                    newSet = newSet.rotate(false);
-//                } else {
-//                    //right left case
-//                    newSet = new AVLTree(newSet.getRight().rotate(true),
-//                                         newSet.getLeft(),
-//                                         newSet.getData()).rotate(false);
-//                }
-//            }
-//            return newSet;
-//        } else {
-//            return tree;
-//        }
-//    }
-    
-    
     
     
 }
